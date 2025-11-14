99096b63672ede62c3cb9f93136ee73620ce6c4b
SaiSai <sais27160@gmail.com>
2025-11-14 19:15:21 +0100

    Add Tic Tac Toe game implementation and game hub interface

---
+++ project.py
@@ -0,0 +1,32 @@
+
+# --- The grid ---
+def draw_grid():
+    .......
+
+# --- Draw X or O ---
+def draw_symbol():
+    .......
+
+
+# --- Check Win ---
+def check_win():
+    ......
+
+# --- Check Draw ---
+def check_draw():
+    
+
+# --- Draw Winning Line ---
+def draw_win_line():
+    .......
+    
+    
+# --- Click Handler ---
+def click():
+    ......
+    
+# --- Restart Game ---
+def restart_game():
+    ........
+   
+mainloop()
