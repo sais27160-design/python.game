7584d2de3fc6ecc1bf3586e8db86c82601c50a7f
sais27160-design <sais27160@gmail.com>
2025-11-20 14:53:25 +0100

    Update homescreen.py

--- homescreen.py
+++ homescreen.py
@@ -7,27 +7,27 @@ def open_tictactoe():
 
 
 #Window 
-root = tk.Tk()
-root.title("Game Hub")
-root.geometry("800x800")
-root.config(bg="lightblue")
+game = tk.Tk()
+game.title("Game Hub")
+game.geometry("800x800")
+game.config(bg="lightblue")
 
 #Title 
-title = tk.Label(root, text="GAME MENU", font=("Arial", 24), bg="lightblue")
+title = tk.Label(game, text="GAME MENU", font=("Arial", 24), bg="lightblue")
 title.pack(pady=40)
 
 #Buttons 
-play_btn = tk.Button(root, text="Play Tic Tac Toe", font=("Arial", 16),
+play_btn = tk.Button(game, text="Play Tic Tac Toe", font=("Arial", 16),
                      width=18, command=open_tictactoe)
 play_btn.pack(pady=15)
 
-choose_btn = tk.Button(root, text="Sneak and ladder", font=("Arial", 16),
+choose_btn = tk.Button(game, text="Sneak and ladder", font=("Arial", 16),
                        width=18)
 choose_btn.pack(pady=15)
 
-exit_btn = tk.Button(root, text="Exit", font=("Arial", 16),
-                     width=18, command=root.destroy)
+exit_btn = tk.Button(game, text="Exit", font=("Arial", 16),
+                     width=18, command=game.destroy)
 exit_btn.pack(pady=15)
 
 
-root.mainloop()
+game.mainloop()
